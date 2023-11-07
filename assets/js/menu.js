212a837e1b4c1754f2bb94a55a39968781c87069
ngoc160304 <ngocdev160304@gmail.com>
2023-11-07 23:07:51 +0700

    fix header bar

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -11,6 +11,14 @@ for(let i = 0; i < listMenu.length; i++) {
 const header = document.querySelector(".header .inner-header");
 const iconMenuBars = document.querySelector(".inner-bar");
 const menuBars = document.querySelector(".inner-list-menu-bars");
+var onresize = function() {
+    width = document.body.clientWidth;
+    if(width >= 978) {
+        menuBars.classList.remove("dp-block");
+        header.classList.remove("border-bottom")
+    }
+}
+window.addEventListener("resize", onresize);
 iconMenuBars.addEventListener("click", () => {
     menuBars.classList.toggle("dp-block");
     header.classList.toggle("border-bottom");
